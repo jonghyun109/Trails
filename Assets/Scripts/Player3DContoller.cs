8477aadd398d703df7281d199f740008fe8b4ec1
jonghyun109 <mimj2527@gmail.com>
2025-07-15 01:17:59 +0900

    [feat] 애니메이션 추가

--- Assets/Scripts/Player3DContoller.cs
+++ Assets/Scripts/Player3DContoller.cs
@@ -7,16 +7,19 @@ public class Player3DController : WalkerBase
     public float groundCheckDistance = 0.8f;
 
     private Rigidbody rb;
+    private Animator animator;
+
     public bool isGrounded;
 
     void Start()
     {
-        if(photonView.IsMine)
+        if (photonView.IsMine)
         {
             rb = GetComponent<Rigidbody>();
             rb.freezeRotation = true;
+            animator = GetComponentInChildren<Animator>();
         }
-        else if (!photonView.IsMine)
+        else
         {
             GetComponent<Rigidbody>().isKinematic = true;
         }
@@ -24,32 +27,38 @@ public class Player3DController : WalkerBase
 
     void Update()
     {
-        if (photonView.IsMine)
+        if (!photonView.IsMine) return;
+
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Direction = new Vector3(h, 0, v);
+        Walk(Direction);
+
+        // 걷기 애니메이션
+        bool isWalking = Direction.magnitude > 0;
+        animator.SetBool("Walk", isWalking);
+
+        // 좌우 반전
+        if (h != 0)
         {
-            float h = Input.GetAxisRaw("Horizontal");
-            float v = Input.GetAxisRaw("Vertical");
-
-            Direction = new Vector3(h, 0, v);
-            Walk(Direction);
-
-            CheckGround();
-
-            if (Input.GetKeyDown(KeyCode.LeftShift))
-            {
-                MoveSpeed = MoveSpeed * 1.3f;
-            }
-            else if (Input.GetKeyUp(KeyCode.LeftShift))
-            {
-                MoveSpeed = 10;
-            }
-
-
-            if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-            {
-                Jump();
-            }
+            Vector3 scale = transform.localScale;
+            scale.x = Mathf.Sign(h) * Mathf.Abs(scale.x); // 왼쪽이면 음수, 오른쪽이면 양수
+            transform.localScale = scale;
         }
-        
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            MoveSpeed *= 1.3f;
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+            MoveSpeed = 10;
+
+        // 점프
+        CheckGround();
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+            animator.SetTrigger("Jump");
+        }
     }
 
     protected override void Move(Vector3 movement)
@@ -67,5 +76,4 @@ public class Player3DController : WalkerBase
     {
         isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance + 0.1f, groundLayer);
     }
-
 }
